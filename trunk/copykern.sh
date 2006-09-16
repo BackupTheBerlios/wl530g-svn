0e2ffae501aae1b13ce75cbc1f2fb25b88a5a101
jcollake <jcollake@758947cf-f81c-0410-876c-b5cc3e67f443>
2006-09-16 00:32:55 +0000

    a little script to copy the kernel to the firmware_files intermediate folder

---
+++ trunk/copykern.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+cp Marvell/Src/uClinux-dist/linux-2.4.x/arch/armnommu/boot/zImage firmware_files/image_parts/segment1
+cp Marvell/Src/uClinux-dist/linux-2.4.x/arch/armnommu/boot/zImage firmware_files/image_parts/zImage
+
+
