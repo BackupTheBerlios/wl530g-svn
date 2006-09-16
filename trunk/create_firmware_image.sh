ef62ff3d836409dbb6188a51cc26a384c71ab07a
jcollake <jcollake@758947cf-f81c-0410-876c-b5cc3e67f443>
2006-09-16 14:20:29 +0000

    * now include all firmware_files  * devices are stored in a tar.gz archive and extracted when the firmware image is built.  * change create_firmware_image.sh appropriately.  * removed extract_latest.sh since it's no longer useful.
    Now to edit the base files you simply need to change the files in firmware_files/rootfs.

--- trunk/create_firmware_image.sh
+++ trunk/create_firmware_image.sh
@@ -4,6 +4,13 @@
 #
 current_date=$(date +%m%d%y)
 OUTPUT_FILE=wl530g_db90h_"$current_date"_en.bin
+echo " Final output image will be $OUTPUT_FILE"
+echo " Log for $OUTPUT_FILE" > build.log
+echo " Extracting devices.."
+cd firmware_files
+tar -xzvf dev.tar.gz -C rootfs/ 2>&1 >> ../build.log
+cd ..
+echo " Building firmware image.."
 cd firmware-mod-kit
 ./build_firmware.sh ../firmware_images/temp ../firmware_files
 cd ..
@@ -11,7 +18,11 @@ if [ -f "firmware_images/$OUTPUT_FILE" ]; then
 	echo " WARNING: over-writing old $OUTPUT_FILE"
 	rm "firmware_images/$OUTPUT_FILE"
 fi
+# delete devices again..
+rm -rf firmware_files/rootfs/dev
 mv firmware_images/temp/custom_image-wl530g.trx firmware_images/$OUTPUT_FILE
 rm firmware_images/latest_db90h_firmware_image.bin
-ln -s firmware_images/$OUTPUT_FILE latest_db90h_firmware_image.bin
+cd firmware_images
+ln -s $OUTPUT_FILE latest_db90h_firmware_image.bin
+cd ..
 rm -rf firmware_images/temp
--- trunk/extract_latest.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-cd firmware-mod-kit
-./extract_firmware.sh ../firmware_images/latest_db90h_firmware_image.bin ../firmware_files
