a8fa5f57fbcc337cee71ef389f70c925cdd35a3c
jcollake <jcollake@758947cf-f81c-0410-876c-b5cc3e67f443>
2006-09-17 03:07:56 +0000

    added code to remove .svn folders before building firmware (a good idea, eh?)

--- trunk/create_firmware_image.sh
+++ trunk/create_firmware_image.sh
@@ -6,13 +6,19 @@ current_date=$(date +%m%d%y)
 OUTPUT_FILE=wl530g_db90h_"$current_date"_en.bin
 echo " Final output image will be $OUTPUT_FILE"
 echo " Log for $OUTPUT_FILE" > build.log
-echo " Extracting devices.."
+echo " Extracting devices ..."
 cd firmware_files
 tar -xzvf dev.tar.gz -C rootfs/ 2>&1 >> ../build.log
 cd ..
+echo " Copying filesystem and deleting svn files ..."
+rm -rf build_temp
+cp -r firmware_files build_temp
+for i in $(find build_temp/ | grep .svn); do
+	rm -rf $i
+done
 echo " Building firmware image.."
 cd firmware-mod-kit
-./build_firmware.sh ../firmware_images/temp ../firmware_files
+./build_firmware.sh ../firmware_images/temp ../build_temp
 cd ..
 if [ -f "firmware_images/$OUTPUT_FILE" ]; then
 	echo " WARNING: over-writing old $OUTPUT_FILE"
@@ -26,3 +32,4 @@ cd firmware_images
 ln -s $OUTPUT_FILE latest_db90h_firmware_image.bin
 cd ..
 rm -rf firmware_images/temp
+rm -rf build_temp
