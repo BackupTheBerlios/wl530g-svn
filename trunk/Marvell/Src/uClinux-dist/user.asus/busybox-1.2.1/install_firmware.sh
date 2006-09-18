60db74b393033b3b8be5ee1c1e3eb5fff4b9a922
jcollake <jcollake@758947cf-f81c-0410-876c-b5cc3e67f443>
2006-09-18 23:23:22 +0000

    make and install stand-alone tools that used to be in busybox after we install busybox

--- trunk/Marvell/Src/uClinux-dist/user.asus/busybox-1.2.1/install_firmware.sh
+++ trunk/Marvell/Src/uClinux-dist/user.asus/busybox-1.2.1/install_firmware.sh
@@ -4,3 +4,10 @@ echo "Deleting all existing files in bin/ ..."
 rm ../../../../../firmware_files/rootfs/bin/*
 echo "Installing busybox ..."
 make install PREFIX=../../../../../firmware_files/rootfs/
+cd ../../user/shutils
+make
+./installall.sh
+cd ../fileutils
+make
+./installall.sh
+cd ../../user.asus/busybox-1.2.1
