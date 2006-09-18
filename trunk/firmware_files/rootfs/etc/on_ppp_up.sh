6cc4bc04f950a2d26519e2c1dd842a53b5d2e467
jcollake <jcollake@758947cf-f81c-0410-876c-b5cc3e67f443>
2006-09-18 17:50:39 +0000

    new scripts rc invokes when a ppp link goes up/down. These scripts in turn invoke /jffs/etc/on_ppp_{up|down} .

---
+++ trunk/firmware_files/rootfs/etc/on_ppp_up.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+#
+# called when PPP link is established.
+#
+echo "$0: started" | logger -s -p 6 -t '' &
+# invoke /jffs/etc/on_ppp_up.sh if it exists
+if [ -f "/jffs/etc/on_ppp_up.sh" ]; then
+	/jffs/etc/on_ppp_up.sh &
+fi
+echo "$0: finished" | logger -s -p 6 -t '' &
