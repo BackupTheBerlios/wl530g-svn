aff27ce2095f16a36e7f86dc6f2b47da62cb5635
jcollake <jcollake@758947cf-f81c-0410-876c-b5cc3e67f443>
2006-09-18 00:50:54 +0000

    delete all files in bin/ before installing busybox. The idea is to only use bin/ for busybox so that we can more easily clean it out.

--- trunk/Marvell/Src/uClinux-dist/user.asus/busybox-1.2.1/install_firmware.sh
+++ trunk/Marvell/Src/uClinux-dist/user.asus/busybox-1.2.1/install_firmware.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 echo "Installing to firmware filesystem ..."
+echo "Deleting all existing files in bin/ ..."
+rm ../../../../../firmware_files/rootfs/bin/*
+echo "Installing busybox ..."
 make install PREFIX=../../../../../firmware_files/rootfs/
