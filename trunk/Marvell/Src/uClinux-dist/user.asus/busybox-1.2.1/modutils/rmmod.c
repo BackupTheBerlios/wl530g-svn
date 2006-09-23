2d21771b5114715565968987af6988cc9412ddb5
jcollake <jcollake@758947cf-f81c-0410-876c-b5cc3e67f443>
2006-09-23 01:38:58 +0000

    backported bb 0.60's rmmod to remove missing __NR_delete_module. Also ended up smaller

--- trunk/Marvell/Src/uClinux-dist/user.asus/busybox-1.2.1/modutils/rmmod.c
+++ trunk/Marvell/Src/uClinux-dist/user.asus/busybox-1.2.1/modutils/rmmod.c
@@ -2,105 +2,63 @@
 /*
  * Mini rmmod implementation for busybox
  *
- * Copyright (C) 1999-2004 by Erik Andersen <andersen@codepoet.org>
+ * Copyright (C) 1999,2000 by Lineo, inc. and Erik Andersen
+ * Copyright (C) 1999,2000,2001 by Erik Andersen <andersee@debian.org>
  *
- * Licensed under GPLv2 or later, see file LICENSE in this tarball for details.
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ *
  */
 
-#include "busybox.h"
 #include <stdio.h>
 #include <errno.h>
 #include <unistd.h>
 #include <stdlib.h>
 #include <getopt.h>
-#include <fcntl.h>
-#include <string.h>
-#include <sys/utsname.h>
-#include <sys/syscall.h>
+#include "busybox.h"
 
-#ifdef CONFIG_FEATURE_2_6_MODULES
-static inline void filename2modname(char *modname, const char *afterslash)
-{
-	unsigned int i;
+/* fixes for the move from 0.60 to 1.2.1 */
+#define show_usage bb_show_usage
+#define perror_msg bb_perror_msg 
+#define perror_msg_and_die bb_perror_msg_and_die
 
-#if ENABLE_FEATURE_2_4_MODULES
-	int kr_chk = 1;
-	if (get_linux_version_code() <= KERNEL_VERSION(2,6,0))
-		kr_chk = 0;
-#else
-#define kr_chk 1
-#endif
+extern int delete_module(const char * name);
 
-	/* Convert to underscores, stop at first . */
-	for (i = 0; afterslash[i] && afterslash[i] != '.'; i++) {
-		if (kr_chk && (afterslash[i] == '-'))
-			modname[i] = '_';
-		else
-			modname[i] = afterslash[i];
-	}
-	modname[i] = '\0';
-}
-#endif
 
-int rmmod_main(int argc, char **argv)
+extern int rmmod_main(int argc, char **argv)
 {
 	int n, ret = EXIT_SUCCESS;
-	unsigned int flags = O_NONBLOCK|O_EXCL;
-#ifdef CONFIG_FEATURE_QUERY_MODULE_INTERFACE
-	/* bb_common_bufsiz1 hold the module names which we ignore
-	   but must get */
-	size_t bufsize = sizeof(bb_common_bufsiz1);
-#endif
 
 	/* Parse command line. */
-	n = bb_getopt_ulflags(argc, argv, "wfa");
-	if((n & 1))	// --wait
-		flags &= ~O_NONBLOCK;
-	if((n & 2))	// --force
-		flags |= O_TRUNC;
-	if((n & 4)) {
-		/* Unload _all_ unused modules via NULL delete_module() call */
-		/* until the number of modules does not change */
-		size_t nmod = 0; /* number of modules */
-		size_t pnmod = -1; /* previous number of modules */
-
-		while (nmod != pnmod) {
-			if (syscall(__NR_delete_module, NULL, flags) != 0) {
-				if (errno==EFAULT)
-					return(ret);
-				bb_perror_msg_and_die("rmmod");
-			}
-			pnmod = nmod;
-#ifdef CONFIG_FEATURE_QUERY_MODULE_INTERFACE
-			/* 1 == QM_MODULES */
-			if (my_query_module(NULL, 1, &bb_common_bufsiz1, &bufsize, &nmod)) {
-				bb_perror_msg_and_die("QM_MODULES");
-			}
-#endif
+	while ((n = getopt(argc, argv, "a")) != EOF) {
+		switch (n) {
+			case 'a':
+				/* Unload _all_ unused modules via NULL delete_module() call */
+				if (delete_module(NULL))
+					perror_msg_and_die("rmmod");
+				return EXIT_SUCCESS;
+			default:
+				show_usage();
 		}
-		return EXIT_SUCCESS;
 	}
 
 	if (optind == argc)
-		bb_show_usage();
+			show_usage();
 
 	for (n = optind; n < argc; n++) {
-#ifdef CONFIG_FEATURE_2_6_MODULES
-		const char *afterslash;
-		char *module_name;
-
-		afterslash = strrchr(argv[n], '/');
-		if (!afterslash)
-			afterslash = argv[n];
-		else
-			afterslash++;
-		module_name = alloca(strlen(afterslash) + 1);
-		filename2modname(module_name, afterslash);
-#else
-#define module_name		argv[n]
-#endif
-		if (syscall(__NR_delete_module, module_name, flags) != 0) {
-			bb_perror_msg("%s", argv[n]);
+		if (delete_module(argv[n]) < 0) {
+			perror_msg("%s", argv[n]);
 			ret = EXIT_FAILURE;
 		}
 	}
